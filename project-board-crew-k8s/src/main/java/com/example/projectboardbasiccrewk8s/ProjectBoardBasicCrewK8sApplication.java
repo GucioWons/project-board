357cceb93b78ea6025392404943651602d1e0450
KubaBart <256466@student.pwr.edu.pl>
2023-04-12 20:55:00 +0200

    Refactor

---
+++ project-board-crew-k8s/src/main/java/com/example/projectboardbasiccrewk8s/ProjectBoardBasicCrewK8sApplication.java
@@ -0,0 +1,13 @@
+package com.example.projectboardbasiccrewk8s;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectBoardBasicCrewK8sApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectBoardBasicCrewK8sApplication.class, args);
+    }
+
+}
