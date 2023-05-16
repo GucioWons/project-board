84699c8489f86d62fbb8e3af5144b401bbc0fb58
GucioWons <wgutek1@wp.pl>
2023-05-16 20:40:31 +0200

    Handling SQL errors

--- Crew/src/main/java/project/board/crew/logic/exceptions/ApiExceptionHandler.java
+++ Crew/src/main/java/project/board/crew/logic/exceptions/ApiExceptionHandler.java
@@ -1,5 +1,6 @@
 package project.board.crew.logic.exceptions;
 
+import org.hibernate.PropertyValueException;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -8,10 +9,22 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+import java.sql.SQLIntegrityConstraintViolationException;
+
 @ControllerAdvice
 public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
     @ExceptionHandler(value = { ApiException.class })
-    protected ResponseEntity<Object> handleConflict(ApiException e, WebRequest request) {
+    protected ResponseEntity<Object> handleApiConflict(ApiException e, WebRequest request) {
         return handleExceptionInternal(e, e.getMessage(), new HttpHeaders(), HttpStatus.NOT_FOUND, request);
+    }
+
+    @ExceptionHandler(value = {PropertyValueException.class})
+    protected ResponseEntity<Object> handleValueConflict(PropertyValueException e, WebRequest request){
+        return handleExceptionInternal(e, "One or more values are incorrect!", new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
+    }
+
+    @ExceptionHandler(value = {SQLIntegrityConstraintViolationException.class})
+    protected ResponseEntity<Object> handleConstraintConflict(SQLIntegrityConstraintViolationException e, WebRequest request){
+        return handleExceptionInternal(e, "Constraint violation! Make sure that unique values are unique!", new HttpHeaders(), HttpStatus.BAD_REQUEST, request);
     }
 }
